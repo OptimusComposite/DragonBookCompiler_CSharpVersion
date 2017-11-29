0d5d56bebeff3ca1f1cbb8faf9d7546ccd30a7ec
Phi <optimusgrass@live.com>
2017-11-29 22:24:26 +0800

    Hi

--- DragonBookCompiler.Generic/Lexer/Lexer.cs
+++ DragonBookCompiler.Generic/Lexer/Lexer.cs
@@ -4,13 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Collections;
+using DragonBookCompiler.Generic.Symbols;
+using System.IO;
 
 namespace DragonBookCompiler.Generic.Lexer
 {
     public class Lexer
     {
         public static int Line = 1;
-        char peek = ' ';
+        char Peek = ' ';
         Hashtable Words = new Hashtable();
         void Reserve(Word w)
         {
@@ -18,11 +20,44 @@ namespace DragonBookCompiler.Generic.Lexer
         }
         public Lexer()
         {
-            Reserve(new Word("if", Tag.IF));
-            Reserve(new Word("else", Tag.ELSE));
-            Reserve(new Word("while", Tag.WHILE));
-            Reserve(new Word("do", Tag.DO));
-            Reserve(new Word("break", Tag.BREAK));
+            Reserve(new Word("if",      Tag.IF      ));
+            Reserve(new Word("else",    Tag.ELSE    ));
+            Reserve(new Word("while",   Tag.WHILE   ));
+            Reserve(new Word("do",      Tag.DO      ));
+            Reserve(new Word("break",   Tag.BREAK   ));
+            Reserve(Word.True);
+            Reserve(Word.False);
+            Reserve(Symbols.Type.Int);
+            Reserve(Symbols.Type.Char);
+            Reserve(Symbols.Type.Float);
+            Reserve(Symbols.Type.Bool);
         }
+        void ReadChar()
+        {
+            try
+            {
+                Peek = (char)Console.Read();
+            }
+            catch
+            {
+                throw new IOException();
+            }
+        }
+        Boolean ReadChar(char c)
+        {
+            try
+            {
+                ReadChar();
+                if (Peek != c)
+                    return false;
+                Peek = ' ';
+                return true;
+            }
+            catch
+            {
+                throw new IOException();
+            }
+        }
+
     }
 }
---
+++ DragonBookCompiler.Generic/Symbols/Type.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DragonBookCompiler.Generic.Lexer;
+
+namespace DragonBookCompiler.Generic.Symbols
+{
+    public class Type : Word
+    {
+        public int Width = 0;
+
+        public Type(string s, int tag, int w) : base(s, tag)
+        {
+            Width = w;
+        }
+
+        public static readonly Type Int     = new Type("int",   Tag.BASIC, 4),
+                                    Float   = new Type("float", Tag.BASIC, 8),
+                                    Char    = new Type("char",  Tag.BASIC, 1),
+                                    Bool    = new Type("bool",  Tag.BASIC, 1);
+
+        public static Boolean Numeric (Type p)
+        {
+            if (p == Char || p == Float || p == Int)
+                return true;
+            else
+                return false;
+        }
+
+        public static Type Max(Type p1, Type p2)
+        {
+            if (!(Numeric(p1)) || !(Numeric(p2)))
+                return null;
+            else if (p1 == Float || p2 == Float)
+                return Float;
+            else if (p1 == Int || p2 == Int)
+                return Int;
+            else
+                return Char;
+        }
+    }
+}
