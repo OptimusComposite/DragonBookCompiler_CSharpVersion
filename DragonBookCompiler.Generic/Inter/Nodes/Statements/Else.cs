da4922ea24435a385dbb71fe091b1851334ff491
Phi <optimusgrass@live.com>
2017-12-13 22:58:12 +0800

    ...

--- DragonBookCompiler.Generic/Inter/Nodes/Statements/Else.cs
+++ DragonBookCompiler.Generic/Inter/Nodes/Statements/Else.cs
@@ -3,10 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DragonBookCompiler.Generic.Symbols;
 
 namespace DragonBookCompiler.Generic.Inter
 { 
     public class Else : Statement
     {
+        Express ex;
+        Statement st1, st2;
+
+        public Else(Express e, Statement s1, Statement s2)
+        {
+            ex = e;
+            st1 = s1;
+            st2 = s2;
+            if (ex.type != Symbols.Type.Bool)
+                ex.Error("Boolean required in if");
+        }
+
+        public new void Gen(int b, int a)
+        {
+            int label1 = NewLabel();
+            int label2 = NewLabel();
+            ex.Jump(0, label2);
+            EmitLabel(label1);
+            st1.Gen(label1, a);
+            Emit("Goto L" + a);
+            EmitLabel(label2);
+            st2.Gen(label2, a);
+        }
     }
 }
--- DragonBookCompiler.Generic/Inter/Nodes/Statements/If.cs
+++ DragonBookCompiler.Generic/Inter/Nodes/Statements/If.cs
@@ -8,5 +8,23 @@ namespace DragonBookCompiler.Generic.Inter
 { 
     public class If : Statement
     {
+        Express ex;
+        Statement st;
+
+        public If(Express e, Statement s)
+        {
+            ex = e;
+            st = s;
+            if (ex.type != Symbols.Type.Bool)
+                ex.Error("Boolean required in if");
+        }
+
+        public new void Gen(int b, int a)
+        {
+            int label = NewLabel();
+            ex.Jump(0, a);
+            EmitLabel(label);
+            st.Gen(label, a);
+        }
     }
 }
