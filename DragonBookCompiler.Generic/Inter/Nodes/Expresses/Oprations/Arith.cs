73a7f26cbf72ab5e4378f7b6363068da5632c062
Phi <optimusgrass@live.com>
2017-12-15 22:19:15 +0800

    ...

--- DragonBookCompiler.Generic/Inter/Nodes/Expresses/Oprations/Arith.cs
+++ DragonBookCompiler.Generic/Inter/Nodes/Expresses/Oprations/Arith.cs
@@ -3,10 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DragonBookCompiler.Generic.Lexer;
+using DragonBookCompiler.Generic.Symbols;
 
 namespace DragonBookCompiler.Generic.Inter
 {
     public class Arith : Operation
     {
+        public Express expr1, expr2;
+
+        public Arith(Token tok, Express x1, Express x2) : base(tok, null)
+        {
+            expr1 = x1;
+            expr2 = x2;
+            type = Symbols.Type.Max(expr1.type, expr2.type);
+            if (type == null)
+                Error("type error");
+        }
+
+        public new Express Gen()
+        {
+            return new Arith(Op, expr1.Reduce(), expr2.Reduce());
+        }
+
+        public override String ToString()
+        {
+            return expr1.ToString() + " " + Op.ToString() + " " + expr2.ToString();
+        }
     }
 }
--- DragonBookCompiler.Generic/Inter/Nodes/Expresses/Oprations/Unary.cs
+++ DragonBookCompiler.Generic/Inter/Nodes/Expresses/Oprations/Unary.cs
@@ -3,10 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DragonBookCompiler.Generic.Lexer;
+using DragonBookCompiler.Generic.Symbols;
 
 namespace DragonBookCompiler.Generic.Inter
 {
     public class Unary : Operation
     {
+        public Express expr;
+
+        public Unary(Token tok, Express x) : base(tok, null)
+        {
+            expr = x;
+            type = Symbols.Type.Max(Symbols.Type.Int, expr.type);
+            if (type == null)
+                Error("type error");
+        }
+
+        public new Express Gen()
+        {
+            return new Unary(Op, expr.Reduce());
+        }
+
+        public override String ToString()
+        {
+            return Op.ToString() + " " + expr.ToString();
+        }
     }
 }
